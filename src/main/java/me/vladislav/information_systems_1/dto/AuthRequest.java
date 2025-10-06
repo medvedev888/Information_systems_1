4e71a5f50f7892de535e6a390a6750cc011ba37d
vladislavmedvedev <EnoT3581@mail.ru>
2025-10-06 03:53:08 +0300

    feat: added UserDTO.java, AuthRequest.java and AuthResponse.java

---
+++ src/main/java/me/vladislav/information_systems_1/dto/AuthRequest.java
@@ -0,0 +1,13 @@
+package me.vladislav.information_systems_1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    public String login;
+    public String password;
+}
---
+++ src/main/java/me/vladislav/information_systems_1/dto/AuthResponse.java
@@ -0,0 +1,13 @@
+package me.vladislav.information_systems_1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private UserDTO userDTO;
+}
