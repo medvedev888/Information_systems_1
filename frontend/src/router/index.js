11471c04a2ad34ae8dc2e1900077da2e7e79d8cf
vladislavmedvedev <EnoT3581@mail.ru>
2025-10-09 22:01:52 +0300

    refactor: added route guard to redirect unauthenticated users to login page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,15 +5,24 @@ import Login from '../pages/Login.vue'
 import SpecialOperations from '../pages/SpecialOperations.vue'
 
 const routes = [
-  {path: "/", name: "Home", component: Home},
+  { path: "/", name: "Home", component: Home, meta: { requiresAuth: true } },
   {path: "/register", name: "Register", component: Register},
   {path: "/login", name: "Login", component: Login},
-  {path: "/operations", name: "SpecialOperations", component: SpecialOperations}
+  {path: "/operations", name: "SpecialOperations", component: SpecialOperations, meta: { requiresAuth: true }}
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("jwt");
+  if (to.meta.requiresAuth && !token) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 export default router
