d8f0b7681fb347d9c7594a4fba48f7369ce20619
vladislavmedvedev <EnoT3581@mail.ru>
2025-10-09 22:00:42 +0300

    feat: added link.vue

---
+++ frontend/src/components/link.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  label: { type: String, default: "Go to link" },
+  href: { type: String, default: "#" }
+})
+defineEmits(["click"])
+</script>
+
+<template>
+  <a
+    :href="href"
+    class="link"
+    @click="$emit('click', $event)"
+  >
+    {{ label }}
+  </a>
+</template>
+
+<style scoped>
+.link {
+
+  display: inline-block;
+  padding: 0;
+  color: black;
+  text-decoration: none;
+  border-radius: 0.375rem;
+  cursor: pointer;
+  font-weight: 300;
+  transition: background 0.2s;
+}
+
+.link:hover {
+  color: rgba(79, 70, 229, 0.3);
+}
+</style>
