595c36a23f6327df6d37ac7c6a3e21baad65fe14
vladislavmedvedev <EnoT3581@mail.ru>
2025-10-14 00:42:16 +0300

    feat: add operations column support with action buttons in Table component

--- frontend/src/components/table.vue
+++ frontend/src/components/table.vue
@@ -1,12 +1,13 @@
 <script setup>
-import {defineEmits, defineProps} from "vue";
+import { defineEmits, defineProps } from "vue";
+import Button from "@/components/button.vue";
 
 const props = defineProps({
   columns: Array,
   rows: Array
 });
-const emit = defineEmits(["rowClick"]);
 
+const emit = defineEmits(["rowClick"]);
 </script>
 
 <template>
@@ -17,8 +18,23 @@ const emit = defineEmits(["rowClick"]);
     </tr>
     </thead>
     <tbody>
-    <tr v-for="(row, index) in rows" :key="index" @click="emit('rowClick', row)">
-      <td v-for="col in columns" :key="col.key">{{ row[col.key] }}</td>
+    <tr v-for="(row, rowIndex) in rows" :key="rowIndex" @click="emit('rowClick', row)">
+      <td v-for="col in columns" :key="col.key" class="td">
+        <template v-if="col.key === 'operations' && row[col.key]">
+          <div  class="button-container">
+            <Button
+              v-for="btn in row[col.key]"
+              :key="btn.type"
+              :label="btn.type"
+              :icon="btn.icon"
+              @click="btn.onClick"
+            />
+          </div>
+        </template>
+        <template v-else>
+          {{ row[col.key] }}
+        </template>
+      </td>
     </tr>
     </tbody>
   </table>
@@ -47,5 +63,15 @@ tr:hover {
 .header-row th {
   border-collapse: collapse;
   border-bottom: 1px solid black;
+}
+
+td Button {
+  padding: 0.7rem;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: row;
+  gap: 0.2rem;
 }
 </style>
